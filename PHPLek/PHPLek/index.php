b448b5bf67eb3047614e25e3529a42bb6f84dc6b
MarcusK1990 <MarcusK1990@hotmail.com>
2017-03-13 12:54:09 +0100

    Html document

--- PHPLek/PHPLek/index.php
+++ PHPLek/PHPLek/index.php
@@ -1,4 +1,19 @@
-<?php
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <title>Lek</title>
 
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    </head>
 
-?>
+    <body>
+
+        <?php
+
+        echo utf8_encode("Detta är ett test123!");
+
+        ?>
+
+    </body>
+</html>
